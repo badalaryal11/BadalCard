9ca6ed4c896c241feb3b65e9f37e417f58e41c1d
Badal Aryal <badal.aryal@gmail.com>
2024-02-07 19:49:30 +0545

    Added Custom font

--- BadalCard/ContentView.swift
+++ BadalCard/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Text("Badal Aryal")
+                .font(Font.custom("Pacifico-Regular", size: 40))
+                .bold()
+                .foregroundColor(.white)
         }
-        .padding()
+      
     }
 }
 

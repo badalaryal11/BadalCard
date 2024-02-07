22264125c644c2daa8fb72cabd1c4dc6ff3c8101
Badal Aryal <badal.aryal@gmail.com>
2024-02-07 17:23:55 +0545

    Initial Commit

---
+++ BadalCard/BadalCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadalCardApp.swift
+//  BadalCard
+//
+//  Created by Badal  Aryal on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct BadalCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

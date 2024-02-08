4b112cfb5326a01b6bda55dd1b825d74e2662445
Badal Aryal <badal.aryal@gmail.com>
2024-02-08 16:14:44 +0545

    Extract View into subview and reused it

--- BadalCard/ContentView.swift
+++ BadalCard/ContentView.swift
@@ -12,10 +12,25 @@ struct ContentView: View {
         ZStack{
             Color(red: 0.09, green: 0.63, blue: 0.52)
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Badal Aryal")
-                .font(Font.custom("Pacifico-Regular", size: 40))
-                .bold()
+            VStack {
+                Image("Badal").resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5)
+                )
+                Text("Badal Aryal")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
                 .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+977 986 047 8795", imageName: "phone.fill")
+                InfoView(text: "badal.aryal@gmail.com", imageName: "envelope.fill")
+            }
         }
       
     }
@@ -26,3 +41,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ BadalCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  BadalCard
+//
+//  Created by Badal  Aryal on 08/02/2024.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+        // .foregroundColor(.white)
+            .overlay(HStack { Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+                    .foregroundColor(.black)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
